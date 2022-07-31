8fcab53f539350f87e455e80a7f3aa158aac4459
gracehally7 <99291785+gracehally7@users.noreply.github.com>
2022-07-31 07:32:55 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let operator = prompt('enter the operator:  ');
+let num1 = prompt('enter number 1:  ');
+let num2 = prompt('enter number 1:  ');
+
+if (operator == "+"){
+  console.log(num1 + num2)
+}
+else if(operator == "-"){
+  console.log(num1 - num2)
+}
+ else if(operator == "/")
+{
+  console.log(num1/num2)
+}else if (operator == "*")
+{
+  console.log( num1 * num2)
+}
